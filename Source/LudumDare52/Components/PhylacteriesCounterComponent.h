b3df08679acffb6416283b1af044f50af461a8b0
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-01-07 10:19:03 +0200

    Create component for counting phylacteries

---
+++ Source/LudumDare52/Components/PhylacteriesCounterComponent.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023. Made by Title Goose Team during LudumDare 52. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/SimpleResourceComponent.h"
+#include "PhylacteriesCounterComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE52_API UPhylacteriesCounterComponent : public USimpleResourceComponent
+{
+	GENERATED_BODY()
+};
