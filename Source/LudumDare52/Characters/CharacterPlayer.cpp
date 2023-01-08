c9b1078af149dc0dbfdc2a7931cc5948e27c726f
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-01-08 20:25:09 +0200

    Fix player melee

--- Source/LudumDare52/Characters/CharacterPlayer.cpp
+++ Source/LudumDare52/Characters/CharacterPlayer.cpp
@@ -62,8 +62,11 @@ void ACharacterPlayer::BeginPlay()
 	Super::BeginPlay();
 
 	MeleeAttackComponent->OnAttackFinished.AddDynamic(this, &ACharacterPlayer::FinishAttack);
+	MeleeAttackComponent->OnMeleeBegin.AddDynamic(this, &ACharacterPlayer::HandleMeleeBegin);
+	MeleeAttackComponent->OnMeleeEnd.AddDynamic(this, &ACharacterPlayer::HandleMeleeEnd);
 	RangedAttackComponent->OnAttackFinished.AddDynamic(this, &ACharacterPlayer::FinishAttack);
 	PlayerRestartComponent->OnRestartFinished.AddDynamic(this, &ACharacterPlayer::HandleRestart);
+	
 
 	AGameModeSession* GameModeSession = UTrickyGameModeLibrary::GetSessionGameMode(this);
 
@@ -176,6 +179,16 @@ void ACharacterPlayer::ToggleMovement(const bool bIsEnabled) const
 		PlayerController->SetIgnoreMoveInput(!bIsEnabled);
 		bIsEnabled ? PlayerController->EnableInput(PlayerController) : PlayerController->DisableInput(PlayerController);
 	}
+}
+
+void ACharacterPlayer::HandleMeleeBegin()
+{
+	DamageTriggerComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void ACharacterPlayer::HandleMeleeEnd()
+{
+	DamageTriggerComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ACharacterPlayer::IncrementMaxSouls(const int32 Amount) const
