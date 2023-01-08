6d972c7bdb108efeab113ef82d918b56df8cd6e0
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-01-08 22:48:31 +0200

    Create new enemy class

---
+++ Source/LudumDare52/Characters/CharacterEnemyRanged.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2023. Made by Title Goose Team during LudumDare 52. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterEnemy.h"
+#include "GameFramework/Actor.h"
+#include "CharacterEnemyRanged.generated.h"
+
+UCLASS()
+class LUDUMDARE52_API ACharacterEnemyRanged : public ACharacterEnemy 
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterEnemyRanged();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
